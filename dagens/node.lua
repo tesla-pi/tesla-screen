fb2267747ab92974dfd71b557565d5c2afccd41a
pi <pi>
2017-04-11 16:33:53 +0200

    Continued working on the daily menu. Scraping is finished, set html_to_file for autostart. Writing code in node.lua for display of the menu. REMINDER Print several screens if needed.

--- dagens/node.lua
+++ dagens/node.lua
@@ -2,13 +2,63 @@ gl.setup(1920, 1080)
 local font = resource.load_font("CaviarDreams_Bold.ttf")
 local txt_size = 128
 local meny = resource.load_file("meny.txt")
+local box_width = 64
+local restaurant_color = (1, 1, 1, 1)
+local dish_color = (1, 1, 1, 1)
+local price_color = (1, 1, 1, 1)
+local type_color = (1, 1, 1, 1)
+
 
 node.event("content_update", function(filename, file)
-	if filename == "meny." then
+	if filename == "meny.txt" then
 		meny = resource.load_file(file)
 	end
 end)
 
+function group_and_render(file)
+	local count = 1
+
+	while count < lines do
+		
+		count = count + 1
+	end
+	draw_set(set)
+end
+
+function draw_set(set)
+	local xCount = 0
+	local yCount = 0
+	local maxBoxes = (box_width * txt_size) / WIDTH)
+	maxBoxes = maxBoxes - maxBoxes % 1
+	local temp = set[1]
+	draw_restaurant(0, temp[1])
+
+	for group in set do
+		if xCount > maxBoxes or temp[1] ~= group[1] then
+			yCount = yCount + 1
+			draw_restaurant(yCount*txt_size, group[1])
+			yCount = yCount + 1
+			xCount = 0
+		else
+			draw_box(xCount*box_width*text_size, yCount*text_size*2, group[2], group[3], group[4])
+		end
+		--Draw according to specifications
+		temp = group
+	end
+end
+
+function draw_box(x, y, name, price, type_name)
+	font:draw(x, y, name:sub(1, box_width), txt_size, dish_color)
+	font:draw(x, y + txt_size, type_name:sub(1, (box_width/2)), txt_size, type_color)
+	font:draw(x + (txt_size *(box_width / 2) ), y + txt_size, price, txt_size, price_color)
+end
+
+function draw_restaurant(y, name)
+	font:draw(WIDTH - (txt_size*(name:len()/2)), y, name, txt_size*2, restaurant_color )
+end
+
+
 function node.render()
 	gl.clear(0.1, 0.1, 0.6, 0.4)
+
 end
--- dagens/scraper.py
+++ dagens/scraper.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 #Script is made to get all dishes and related info from Lindholmens dagliga meny.
 import requests
 from bs4 import BeautifulSoup
@@ -8,21 +9,24 @@ weburl = "http://www.lindholmen.se/pa-omradet/dagens-lunch"
 htmls = requests.get(weburl)
 htmltext = htmls.text
 
-uncommented = htmltext.replace("!-- ", "")
+uncommented = htmltext.replace("<!-- ", "")
+uncommented = uncommented.replace("</span> -->", "</span>")
+uncommented = uncommented.replace("\r\n", "")
 soup = BeautifulSoup(uncommented, 'lxml')
 
 menu = soup.find('div', class_="view-content")
-dishes = menu.find_all('div')
+dishes = menu.find_all('div', class_="node-content")
 
-restaurant_name = "a href="
+restaurant_name = "table-list__restaurant-label"
 item_name = ("dish-name")
 price_name = "table-list__column--price"
 type_name = "icon-dish"
 
-print(menu.find('span', class_=item_name).text)
+
 
 #Dishes contain all info needed now, what's left is to sort it and insert all info correctly into a table for display
 for dish in dishes:
-	print(menu.find('span', class_=item_name).text)
-	#print(dish.prettify())
-	#print(dish.find(item_name))
+	print(dish.find('span', class_=restaurant_name).text)
+	print(dish.find('span', class_=item_name).text.rstrip('\n'))
+	print(dish.find('div', class_=price_name).text)
+	print(dish.find('div', class_=type_name).text)
