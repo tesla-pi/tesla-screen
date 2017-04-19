bd2f1bfd8958cfd2e0a3e38b019d4c06cf49e0d0
pi <pi>
2017-04-19 18:58:54 +0200

    Fixed Dagens visualization. TODO implement scroll function for dagens.

--- dagens/node.lua
+++ dagens/node.lua
@@ -2,15 +2,17 @@ gl.setup(1920, 1080)
 local font = resource.load_font("CaviarDreams_Bold.ttf")
 local txt_size = 40
 local meny = resource.load_file("menu.txt")
-local box_width = 30
-
+local box_width = 28
+local offset_y = 200
+local scroll_start = 200
+local scroll_end = 0
 node.event("content_update", function(filename, file)
 	if filename == "menu.txt" then
 		meny = resource.load_file("menu.txt")
 	end
 end)
 
-
+--Splits a textfile by a specific pattern. E.g. x = string.split(file, "\n")
 function string:split(pattern, file)
 	if not file then
 		file = {}
@@ -26,6 +28,7 @@ function string:split(pattern, file)
 	return file
 end
 
+--Groups the info of a file into a matrix and calls the function to draw the set.
 function group_and_render(file)
 	local lines = string.split(file, "\n")
 	local count = 1
@@ -33,15 +36,16 @@ function group_and_render(file)
 	local set = {}
 	while count < table.getn(lines) do
 		set[count] = {}
+		set[count4][count-((count4 -1)*4)] = lines[count]
 		if count % 4 == 0 then
 			count4 = count4 + 1
 		end
-		set[count4][count-((count4 - 1)*4)] = lines[count]
 		count = count + 1
 	end
 	draw_set(set)
 end
 
+--Takes a matrix as a parameter and calls the drawing functions of each grouping.
 function draw_set(set)
 	local xCount = 0
 	local yCount = 1
@@ -51,7 +55,7 @@ function draw_set(set)
 	draw_restaurant(0, temp)
 	local x = 1
 	
-	while x <= (#set) do
+	while x <= (#set/4) do
 		if x == 1 then
 			temp = set[1][1]
 		else
@@ -66,55 +70,76 @@ function draw_set(set)
 			yCount = yCount + 1
 			xCount = 0
 		end
-		draw_box(xCount*box_width*txt_size, yCount*txt_size*2, set[x][2], set[x][3], set[x][0])
+		draw_box(xCount*box_width*txt_size, yCount*txt_size*2, set[x][2], set[x][3], set[x][4])
 		xCount = xCount + 1
 		x = x + 1
 	end
+	set_end(x)
 end
 
+--Used for temporary writing of text
 function log(text)
-	font:write(100,200,text,txt_size*8, 1,1,0,1)
+	font:write(100,200,text,txt_size*2, 1,1,0,1)
 end
 
+--Prints all the info of a meal in a set starting location
 function draw_box(x, y, name, price, type_name)
-	if not name then
-		name = "NO_NAME"
+	if x < WIDTH and y+offset_y < HEIGHT and x >= 0 and y >= 0 then
+		if not name then
+			name = "NO_NAME"
+		end
+		if not price then
+			price = "NO_PRICE"
+		end
+		if not type_name then
+			type_name = "UNKNOWN"
+		end
+		font:write(x, y + offset_y, name:sub(1, box_width*1.5), txt_size, 1,1,1,1)
+	
+		local r = 1
+		local g = 1
+		local b = 0
+		local a = 1
+		if type_name == "Vegetariskt" or type_name == "vegetariskt" or type_name == "Veganskt" or type_name == "veganskt" then
+			r,g,b = 0, 1, 0
+		elseif type_name == "Kött" or type_name == "kött" then
+			r,g,b = 1, 0.2, 0.2
+		elseif type_name == "UNKNOWN" then
+			r,g,b = 1, 0, 0
+		elseif type_name == "Fisk" or type_name == "fisk" then
+			r,g,b = 0.2, 0.2, 0.6
+		elseif type_name == "Pasta" or type_name == "pasta" then
+			r,g,b = 1, 1, 0.2
+		elseif type_name == "Kyckling" or type_name == "kyckling" then
+			r,g,b = 1, 0.8, 0.6
+		else
+			r,g,b = 1, 1, 0
+		end
+		font:write(x, y + txt_size + offset_y, type_name:sub(1, (box_width/2)), txt_size, r, g, b, a)
+		font:write(x + (txt_size *(box_width / 2)+124 ), y + txt_size + offset_y, price, txt_size, 0,1,1,1)
 	end
-	if not price then
-		price = "NO_PRICE"
-	end
-	if not type_name then
-		type_name = "UNKNOWN"
-	end
-	font:write(x, y, name:sub(1, box_width), txt_size, 1,1,1,1)
-	if type_name == "Vegetariskt" or type_name == "vegetariskt" or type_name == "Veganskt" or type_name == "veganskt" then
-		font:write(x, y + txt_size, type_name:sub(1, (box_width/2)), txt_size, 0,1,0,1)
-	elseif type_name == "Kött" or type_name == "kött" then
-		font:write(x, y + txt_size, type_name:sub(1, (box_width/2)), txt_size, 1,0.2,0.2,1)
-	elseif type_name == "UNKNOWN" then
-		font:write(x, y + txt_size, type_name:sub(1, (box_width/2)), txt_size, 1, 0, 0, 1)
-	elseif type_name == "Fisk" or type_name == "fisk" then
-		font:write(x, y + txt_size, type_name:sub(1, (box_width/2)), txt_size, 0.1,0.1,0.6,1)
-	else
-		font:write(x, y + txt_size, type_name:sub(1, (box_width/2)), txt_size, 1,1,0,1)
-	end
-	font:write(x + (txt_size *(box_width / 2)-128 ), y + txt_size, price, txt_size, 0,1,1,1)
 end
 
+
+--Writes the restaurant name centered at a set location
 function draw_restaurant(y, name)
 	if not name then
 		name = "NO_RESTAURANT"
 	end
-	font:write(WIDTH/2 - (txt_size*(name:len()*0.5)), y, name, txt_size*2, 1,1,1,1)
+	font:write(WIDTH/2 - (txt_size*(name:len()*0.5)), y + offset_y, name, txt_size*2, 1,1,1,1)
 end
 
+--Function to scroll the text over time
+function scroll()
+	
+end
+
+--Set the end-offset
+function set_end(count)
+	scroll_end = -(count * 55)
+end
 
 function node.render()
-	gl.clear(0.2,0.2,0.2,1)
+	gl.clear(0.2,0.2,0.2,0.4)
 	group_and_render(meny)
-	--font:write(100, 100, "No Break", txt_size*10, 1,1,1,1)
-	--local temp = string.split(meny, "\n")
-	--for i = 1, #temp do
-	--	font:write(0, i*txt_size, temp[i], txt_size, 1,1,1,1)
-	--end	
 end
--- node.lua
+++ node.lua
@@ -3,19 +3,20 @@ local background = resource.load_image("background.png")
 local font = resource.load_font("CaviarDreams_Bold.ttf")
 local ip = resource.load_file("ip.txt")
 local txt_size = 42
-local nodes = {"welcome", "vader", "bussar"}
+local nodes = {"welcome", "vader", "bussar", "dagens"}
 local node_count = 1
 local timerc = -1
 local timer_init = -1
 local speed = 15
-local sfx_played = 0
 
+--Content update, add cases if more files need to be watched
 node.event("content_update", function(filename, file)
 	if filename == "ip.txt" then
 		ip = resource.load_file("ip.txt")
 	end
 end)
 
+--Switches node to the next one, wrapping around if necessary
 function next_node()
 	if node_count >= table.getn(nodes) then
 		node_count = 1
@@ -24,10 +25,12 @@ function next_node()
 	end
 end
 
+--Sets timer_init to current time
 function timer_start()
 	timer_init = tonumber(os.time())
 end
 
+--Checks timer, if unstarted starts. If timer surpasses speed, restart and switch node
 function timer_check()
 	if timer_init < 0 then
 		timer_start()
@@ -39,14 +42,12 @@ function timer_check()
 	end
 end
 
+--Returns the time difference between timer_init and current time
 function timer_diff()
 	timerc = tonumber(os.time()) - timer_init
 end
 
-function play_sound()
-	sfx_played = 1
-end
-
+--Takes a number and ensures it's within the limits of 60
 function minutify(second)
 	if second > 60 then
 		second = second - 60
@@ -56,6 +57,7 @@ function minutify(second)
 	return second
 end
 
+--Return the node which should be displayed
 function node_choice()
 	local standup_start_hour = 9
 	local standup_start_min = 35
@@ -72,27 +74,23 @@ function node_choice()
 			standup_start_min <= minute and 
 			standup_end_min > minute
 		then
-			play_sound()
 			return "standup"
 		end
 	else
 		if standup_start_hour == hour and 
 			(standup_start_min + 15) <= minute 
 		then
-			play_sound()
 			return "standup"
 		elseif (standup_start_hour + 1) == hour and 
 			minutify(standup_end_min + 15) > minute 
 		then
-			play_sound()
 			return "standup"
 		end
 	end
-
-	sfx_played = 0
 	return nodes[node_count]
 end
 
+--Renders the main node, uses node_choice to get which child-node should be played.
 function node.render()
 	gl.clear(0,0,0,0)
 	timer_check()
@@ -107,7 +105,6 @@ function node.render()
 	local date = year .. "/" .. month .. "/" .. day .. " " .. hour .. ":" .. minute .. ":" .. second
 	
 	font:write(0, HEIGHT - txt_size, ip , txt_size, 1,1,1,1)
-	--font:write(0,0,date, txt_size, 1,0,0,1)
 	
 	resource.render_child(draw_node):draw(0,0,WIDTH, HEIGHT)
 end
